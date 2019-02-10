62f41b61dc221eed67157f8e68b9b4d700c43031
manu <bartosz.manowski@morefromit.com>
2019-02-10 15:32:02 +0100

    gh-pages

---
+++ build/precache-manifest.ae1f7aa25f6d2c7463abcfb83b0760ce.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4960f1be86f458d5c71a194a131a1788",
+    "url": "/todo/static/media/loren.4960f1be.png"
+  },
+  {
+    "revision": "9c91297e17268fbd21d698ca6e526faa",
+    "url": "/todo/static/media/Starjhol.9c91297e.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/todo/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "8a695b30138abc35248a",
+    "url": "/todo/static/js/runtime~main.8a695b30.js"
+  },
+  {
+    "revision": "5e5109f9245496cc7de5",
+    "url": "/todo/static/js/main.5e5109f9.chunk.js"
+  },
+  {
+    "revision": "c9ef396fc61fdf6a49f3",
+    "url": "/todo/static/js/1.c9ef396f.chunk.js"
+  },
+  {
+    "revision": "5e5109f9245496cc7de5",
+    "url": "/todo/static/css/main.67bf2fe7.chunk.css"
+  },
+  {
+    "revision": "121859033f6477ccb8476c0d4bc0430f",
+    "url": "/todo/index.html"
+  }
+];
